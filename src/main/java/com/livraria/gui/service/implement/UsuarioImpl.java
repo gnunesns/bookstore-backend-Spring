c16d1f9544fd73f4f348f01cc99a26f7f3794197
gnunesns <nunesguilherme920@gmail.com>
2022-09-10 22:43:19 -0300

    implement

--- src/main/java/com/livraria/gui/service/implement/UsuarioImpl.java
+++ src/main/java/com/livraria/gui/service/implement/UsuarioImpl.java
@@ -1,7 +1,10 @@
 package com.livraria.gui.service.implement;
 
 import com.livraria.gui.exception.DeleteExceptionUser;
+import com.livraria.gui.model.Aluguel;
 import com.livraria.gui.model.Usuario;
+import com.livraria.gui.model.enums.AluguelStatus;
+import com.livraria.gui.repository.AluguelRepository;
 import com.livraria.gui.repository.UsuarioRepository;
 import com.livraria.gui.service.UsuarioService;
 import org.springframework.data.domain.Page;
@@ -9,21 +12,33 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 
+import static com.livraria.gui.model.enums.AluguelStatus.DELAYED;
+import static com.livraria.gui.model.enums.AluguelStatus.valueOf;
+
 
 @Service
 public class UsuarioImpl implements UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final AluguelRepository aluguelRepository;
 
-    public UsuarioImpl(UsuarioRepository usuarioRepository) {
+    public UsuarioImpl(UsuarioRepository usuarioRepository, AluguelRepository aluguelRepository) {
         this.usuarioRepository = usuarioRepository;
+        this.aluguelRepository = aluguelRepository;
     }
 
     @Override
     public void delete(Usuario usuario) {
-        if(!usuario.getAlugueis().isEmpty()) {
+        /*
+        if(!usuario.getAlugueis().isEmpty()){
             throw new DeleteExceptionUser();
         }
+         */
+        usuario.getAlugueis().forEach(aluguel ->{
+            if (aluguel.getStatus() == AluguelStatus.IN_PROGRESS){
+                throw new DeleteExceptionUser();
+            }
+        });
         usuarioRepository.delete(usuario);
     }
 
